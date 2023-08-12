274cc6a058874451b62f4787e0940e37e403925c
AnJ <140354318+Ajargueta@users.noreply.github.com>
2023-08-11 18:14:38 -0600

    Add files via upload

--- functions.js
+++ functions.js
@@ -1,16 +1,83 @@
-//aqui se va a escribir todas las funciones del programa
-function nineFields(size) {
-  var fieldSize = size / 3;
-  line(fieldSize, 0, fieldSize, size);
-  line(fieldSize * 2, 0, fieldSize * 2, size);
-  line(0, fieldSize, size, fieldSize);
-  line(0, fieldSize * 2, size, fieldSize * 2);
-}
+//esta es la matriz que representa los numeros que se escriben en el grid de sudoku
+var field = [
+  [0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0]
+];
 
-function keyPressed() {
+//esta matriz indica el estado de cada celda en el grid
+//0 es una celda vacia y un numero correcto
+//1 es una celda con un numero escrito al comenzar el juego(no se debe de poder modificar)
+//2 es una celda con un numero erroneo escrito.
+var answers = [
+  [0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0]
+];
+
+var colors = ['#000000', '#6892fa', '#ff5733'];
+var rows = [1,1,1,1,1,1,1,1,1];
+var cols = [1,1,1,1,1,1,1,1,1];
+var fams = [1,1,1,1,1,1,1,1,1];
+var primes = [1,2,3,5,7,11,13,17,19,23]
+
+//la funcion crea numeros aleatorios y hace la verificacion de filas, columnas y cajas usando numeros primos
+function filling_field() {
+  for (var i = 0; i < 9; i++){
+    for(var j = 0; j < 9; j++){
+      //crean un numero entre 0 y 10, para crear una probabilidad de que una casilla se llene con un numero
+      var prob = Math.floor(Math.random() * 11);
+      //esta decision indica que tiene una probabilidad de 4 en 10 de llenarse
+      if (prob > 6){
+        var found = false;
+        var k = i + 1;
+        var tmp1;
+        var tmp2;
+        if((k % 3) === 0){
+          tmp2 = (k / 3) - 1;
+        }else{
+          tmp2 = (k - (k % 3)) / 3;
+        }
+        k = j + 1;
+				if ((k % 3) === 0){
+					tmp1 = (k / 3);
+				}else{
+					tmp1 = Math.floor((k + 3) / 3);
+				}
+				k = (tmp1 + (tmp2 * 3)) - 1;
+				var found = false;
+				var skipper = 1;
+				while(found == false ){
+					var n = Math.floor(Math.random() * 9) + 1;
+					if((rows[i] % primes[n]) !== 0 && (cols[j] % primes[n]) !== 0 && (fams[k] % primes[n]) != 0){
+						rows[i] *= primes[n];
+            cols[j] *= primes[n];
+            fams[k] *= primes[n];
+            answers[i][j] = 1;
+            field[i][j] = n;
+						found = true;
+					}
+          //esta desicion es para evitar un ciclo infinito o uno muy largo, al ser un numero random no se garantiza que se va a crear el numero optimo y puede quedar
+          //atrapado durante un largo tiempo encontranto un numero que no se repita
+					if (skipper === 15){
+						found = true;
+					}
+					skipper++;
+				}
+      }
+    }
+  }
   
-}
-
-function filling_grid() {
-  // put setup code here
 }
--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
-let x;
-let y;
+// run it and click on the sudoku to solve it
+var stepx = 0;
+var stepy = 0;
 
 function nineFields(size) {
   var fieldSize = size / 3;
@@ -9,37 +10,77 @@ function nineFields(size) {
   line(0, fieldSize * 2, size, fieldSize * 2);
 }
 
-function keyPressed() {
-  if (keyCode === UP_ARROW) {
-    y = y - 50;
-  } else if (keyCode === DOWN_ARROW) {
-    y = y + 50;
-  }else if (keyCode === LEFT_ARROW) {
-    x = x - 50;
-  }else if (keyCode === RIGHT_ARROW) {
-    x = x + 50;
+function keyPressed(){
+  var i = stepx / 50;
+  var j = stepy / 50;
+  if ( keyCode === UP_ARROW ) {
+    if ( stepy === 0 ) {
+      stepy = stepy;
+    } else {
+      stepy = stepy - 50;
+    }
+
+  } else if ( keyCode === DOWN_ARROW ) {
+    if ( stepy === 400 ) {
+      stepy = stepy; 
+    } else {
+      stepy = stepy + 50;
+    }
+    
+  } else if ( keyCode === LEFT_ARROW ) {
+    if ( stepx === 0 ) {
+      stepx = stepx;  
+    } else {
+      stepx = stepx - 50;
+    }
+    
+  } else if ( keyCode === RIGHT_ARROW ) {
+    if ( stepx === 400) {
+      stepx = stepx;
+    } else {
+      stepx = stepx + 50;
+    }
+    
+  } 
+
+  if ( keyCode === 97 ) {
+   field[j][i] = 1;
+  } else if ( keyCode === 98 ) {
+    field [j][i] = 2;
+  } else if ( keyCode === 99 ) {
+    field [j][i] = 3;
+  } else if ( keyCode === 100 ) {
+    field [j][i] = 4;
+  } else if ( keyCode === 101 ) {
+     field [j][i] = 5;
+  } else if ( keyCode === 102 ) {
+    field [j][i] = 6;
+  } else if ( keyCode === 103 ) {
+    field [j][i] = 7;
+  } else if ( keyCode === 104 ) {
+    field [j][i] = 8;
+  } else if ( keyCode === 105) {
+    field [j][i] = 9;
   }
-  redraw();
+
 }
 
 function setup() {
   createCanvas(450, 450);
   textSize(25);
-  x = 0;
-  y = 0;
-  noLoop();
+  filling_field();
 }
 
 function draw() {
-  background(246, 222, 250);
-  noFill();
+  background(220);
   strokeWeight(3);
   nineFields(450);
   strokeWeight(1);
+  //noFill();
   push()
-  for (let x = 0; x < 3; x++) {
+  for (var x = 0; x < 3; x++) {
     push()
-    for (let y = 0; y < 3; y++) {
+    for (var y = 0; y < 3; y++) {
       nineFields(150);
       translate(0, 150);
     }
@@ -48,5 +89,22 @@ function draw() {
   }
   pop();
   push();
-  rect(x, y, 49, 49, 10);
+  strokeWeight(1);
+  stroke(colors[1]);
+  for (x = 0; x < 9; x++) {
+    push();
+    for (y = 0; y < 9; y++) {
+      text(field[y][x] ? field[y][x] : '', 20, 35);
+      translate(0, 50);
+    }
+    pop();
+    translate(50, 0);
+  }
+  pop();
+  stroke(0, 0, 0);
+  strokeWeight(2.5);
+  line(stepx, stepy, stepx + 49, stepy);
+  line(stepx + 49, stepy, stepx + 49, stepy + 49);
+  line(stepx + 49, stepy + 49, stepx, stepy +49);
+  line(stepx, stepy + 49, stepx, stepy);
 }
